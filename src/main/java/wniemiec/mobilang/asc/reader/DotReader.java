cf2a540f694eef0336b07ec5b4d7c46c0e0911d0
William Niemiec <williamniemiec@hotmail.com>
2022-01-31 17:44:11 -0300

    Refactoring

--- src/main/java/wniemiec/mobilang/asc/reader/DotReader.java
+++ src/main/java/wniemiec/mobilang/asc/reader/DotReader.java
@@ -8,47 +8,85 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
-
 import com.paypal.digraph.parser.GraphEdge;
 import com.paypal.digraph.parser.GraphNode;
 import com.paypal.digraph.parser.GraphParser;
-
 import wniemiec.mobilang.asc.models.Node;
 
+
+/**
+ * Responsible for reading dot files.
+ */
 public class DotReader {
 
+    //-------------------------------------------------------------------------
+    //		Attributes
+    //-------------------------------------------------------------------------
+    private SortedMap<String, List<Node>> tree;
+
+
+    //-------------------------------------------------------------------------
+    //		Constructor
+    //-------------------------------------------------------------------------
     public DotReader() {
+        tree = new TreeMap<>();
     }
     
-    public SortedMap<String, List<Node>> read(Path filepath) throws FileNotFoundException {
-        SortedMap<String, List<Node>> tree = new TreeMap<>();
-        GraphParser parser = new GraphParser(new FileInputStream(filepath.toFile()));
-        Map<String, GraphEdge> edges = parser.getEdges();
+
+    //-------------------------------------------------------------------------
+    //		Methods
+    //-------------------------------------------------------------------------
+    public void read(Path filepath) throws FileNotFoundException {
+        Map<String, GraphEdge> edges = readEdges(filepath);
+        
         for (GraphEdge edge : edges.values()) {
-            GraphNode nodeLeft = edge.getNode1();
-            GraphNode nodeRight = edge.getNode2();
-            String key = nodeLeft.getId();
-            
-            if (tree.containsKey(key)) {
-                List<Node> children = tree.get(key);
-                children.add(new Node(nodeRight));
-            }
-            else {
-                List<Node> children = new ArrayList<>();
-                children.add(new Node(nodeRight));
-                tree.put(key, children);
-            }
+            parseEdge(edge);
         }
+    }
+
+    private void parseEdge(GraphEdge edge) {
+        GraphNode nodeLeft = edge.getNode1();
+        GraphNode nodeRight = edge.getNode2();
+        String key = nodeLeft.getId();
+        
+        if (tree.containsKey(key)) {
+            addChildToNode(nodeRight, key);
+        }
+        else {
+            addNode(nodeRight, key);
+        }
+    }
+
+    private void addChildToNode(GraphNode child, String nodeId) {
+        List<Node> children = tree.get(nodeId);
+
+        children.add(new Node(child));
+    }
+
+    private void addNode(GraphNode nodeRight, String nodeId) {
+        List<Node> children = new ArrayList<>();
+        
+        children.add(new Node(nodeRight));
+        tree.put(nodeId, children);
+    }
+
+    private Map<String, GraphEdge> readEdges(Path filepath) 
+    throws FileNotFoundException {
+        GraphParser parser = new GraphParser(buildInputStream(filepath));
+        
+        return parser.getEdges();
+    }
+
+    private FileInputStream buildInputStream(Path filepath) 
+    throws FileNotFoundException {
+        return new FileInputStream(filepath.toFile());
+    }
 
 
+    //-------------------------------------------------------------------------
+    //		Getters
+    //-------------------------------------------------------------------------
+    public SortedMap<String, List<Node>> getTree() {
         return tree;
     }
-
-    private int extractNumberFromNode(GraphNode node) {
-        return Integer.parseInt(node.getId().substring(1));
-    }
-
-    private String extractNodeLabel(GraphNode node) {
-        return ((String) node.getAttribute("label"));
-    }   
 }

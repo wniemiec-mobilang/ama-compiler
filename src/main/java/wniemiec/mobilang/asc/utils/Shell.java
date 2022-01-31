f85f21a7ceda792663c8e6fdd6112f89572e152b
William Niemiec <williamniemiec@hotmail.com>
2022-01-31 17:32:34 -0300

    Refactoring

--- src/main/java/wniemiec/mobilang/asc/utils/Shell.java
+++ src/main/java/wniemiec/mobilang/asc/utils/Shell.java
@@ -3,27 +3,80 @@ package wniemiec.mobilang.asc.utils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 
+
+/**
+ * Responsible for running shell commands.
+ */
 public class Shell {
 
+    //-------------------------------------------------------------------------
+    //		Attributes
+    //-------------------------------------------------------------------------
     private Path workingDirectory;
-    private String output = "";
-    private String errorOutput = "";
+    private Runtime runtime;
+    private String output;
+    private String errorOutput;
 
+
+    //-------------------------------------------------------------------------
+    //		Constructors
+    //-------------------------------------------------------------------------
     public Shell() {
         this(Path.of(System.getProperty("user.dir")));
+        runtime = Runtime.getRuntime();
+        output = "";
+        errorOutput = "";
     }
 
     public Shell(Path workingDirectory) {
         this.workingDirectory = workingDirectory;
     }
 
+
+    //-------------------------------------------------------------------------
+    //		Methods
+    //-------------------------------------------------------------------------
     public void exec(String... commands) throws IOException {
-        //System.out.println(System.getProperty("user.dir"));
-        //ProcessBuilder pb = new ProcessBuilder(commands);
-        //Process process = pb.start();
+        Process process = runShell(commands);
+
+        readOutput(process);
+        readErrorOutput(process); 
+    }
+
+    private Process runShell(String... commands) throws IOException {
+        return runtime.exec(
+            normalizeCommands(commands), 
+            null, 
+            workingDirectory.toFile()
+        );
+    }
+
+    private void readOutput(Process process) throws IOException {
+        output = readProcessInputStream(process.getInputStream());
+    }
+
+    private String readProcessInputStream(InputStream stream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder builder = new StringBuilder();
+        String line = null;
+
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+            builder.append(System.getProperty("line.separator"));
+        }
+
+        return builder.toString();
+    }
+
+    private void readErrorOutput(Process process) throws IOException {
+        errorOutput = readProcessInputStream(process.getErrorStream());
+    }
+
+    private String normalizeCommands(String... commands) {
         StringBuilder parsedCommand = new StringBuilder();
 
         for (String command : commands) {
@@ -35,44 +88,22 @@ public class Shell {
             parsedCommand.deleteCharAt(parsedCommand.length()-1);
         }
 
-        System.out.println(parsedCommand.toString());
-        
-        Process process = Runtime.getRuntime().exec(parsedCommand.toString(), null, workingDirectory.toFile());
-        
-        //ProcessBuilder pb = new ProcessBuilder(commands);
-        //pb.directory(workingDirectory.toAbsolutePath().toFile());
-        //Process process = pb.start();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-        StringBuilder builder = new StringBuilder();
-        StringBuilder errorBuilder = new StringBuilder();
-        
-        String line = null;
-        
-        while ( (line = reader.readLine()) != null) {
-            builder.append(line);
-            builder.append(System.getProperty("line.separator"));
-        }
-        String errorLine = null;
-
-        while ( (errorLine = errorReader.readLine()) != null) {
-            errorBuilder.append(errorLine);
-            errorBuilder.append(System.getProperty("line.separator"));
-        }
-
-        output = builder.toString();
-        errorOutput = errorBuilder.toString(); 
+        return parsedCommand.toString();
     }
 
+    public boolean hasError() {
+        return !errorOutput.isBlank();
+    }
+
+
+    //-------------------------------------------------------------------------
+    //		Getters
+    //-------------------------------------------------------------------------
     public String getOutput() {
         return output;
     }
 
     public String getErrorOutput() {
         return errorOutput;
-    }
-
-    public boolean hasError() {
-        return !errorOutput.isBlank();
     }
 }

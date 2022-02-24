876d98027f9490acdb7200ddef26ea516ef3fa8c
William Niemiec <williamniemiec@hotmail.com>
2022-02-24 17:17:29 -0300

    CLI args

--- src/main/java/wniemiec/mobilang/asc/App.java
+++ src/main/java/wniemiec/mobilang/asc/App.java
@@ -1,8 +1,11 @@
 package wniemiec.mobilang.asc;
 
 import java.io.IOException;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
 import wniemiec.io.java.Consolex;
 import wniemiec.mobilang.asc.export.exception.CodeExportException;
 import wniemiec.mobilang.asc.export.exception.OutputLocationException;
@@ -19,9 +22,22 @@ public class App {
     //-------------------------------------------------------------------------
     //		Attributes
     //-------------------------------------------------------------------------
+    private static final String LBL_MOBILANG_AST;
+    private static final String LBL_OUTPUT;
+    private static final String LBL_FRAMEWORK_NAME;
     private static Path mobilangAstFilePath;
     private static Path outputLocationPath;
     private static String frameworkName;
+    
+
+    //-------------------------------------------------------------------------
+    //		Initialization block
+    //-------------------------------------------------------------------------
+    static {
+        LBL_MOBILANG_AST = "mobilang-ast";
+        LBL_OUTPUT = "output";
+        LBL_FRAMEWORK_NAME = "framework";
+    }
 
 
     //-------------------------------------------------------------------------
@@ -32,11 +48,11 @@ public class App {
             parseArgs(args);
             runAsc();
         }
+        catch (IllegalArgumentException e) {
+            Consolex.writeError("Invalid cmd args: " + e.getMessage());
+        }
         catch (FactoryException e) {
             Consolex.writeError("There is no compatibility with this framework: " + frameworkName);
-        }
-        catch (InvalidPathException e) {
-            Consolex.writeError("Invalid dot file location: " + e.getMessage());
         }
         catch (ParseException e) {
             Consolex.writeError("Error while parsing: " + e.getMessage());
@@ -50,7 +66,6 @@ public class App {
         }
         catch (Exception e) {
             Consolex.writeError("Fatal error: " + e.getMessage());
-            e.printStackTrace();
         }
     }
 
@@ -58,19 +73,65 @@ public class App {
     //-------------------------------------------------------------------------
     //		Methods
     //-------------------------------------------------------------------------
-    private static void parseArgs(String[] args) {
-        validateArgs(args);
+    private static void parseArgs(String[] args) 
+    throws ParseException, org.apache.commons.cli.ParseException {
+        CommandLine cmd = buildCmd(args);
 
-        mobilangAstFilePath = Path.of(args[0]);
-        outputLocationPath = Path.of(args[1]);
-        frameworkName = args[2];
+        validateArgs(cmd);
+
+        mobilangAstFilePath = getMobilangAstCliArg(cmd);
+        outputLocationPath = getOutputCliArg(cmd);
+        frameworkName = getFrameworkName(cmd);
     }
 
-    private static void validateArgs(String[] args) {
-        if (args.length < 3) {
-            Consolex.writeError("Missing args! Correct usage: <dot_file> <output_src_location> <framework_name>");
-            System.exit(-1);
+    private static CommandLine buildCmd(String[] args) 
+    throws ParseException, org.apache.commons.cli.ParseException {
+        Options options = buildCliOptions();
+        CommandLineParser parser = new DefaultParser();
+        
+        return parser.parse(options, args);
+    }
+
+    private static Options buildCliOptions() {
+        Options options = new Options();
+
+        options.addOption(LBL_MOBILANG_AST, true, "MobiLang AST file (.dot)");
+        options.addOption(LBL_OUTPUT, true, "Output location");
+        options.addOption(LBL_FRAMEWORK_NAME, true, "Framework name (ex: 'react-native')");
+        
+        return options;
+    }
+
+    private static void validateArgs(CommandLine cmd) {
+        validateCmdOption(cmd, LBL_MOBILANG_AST);
+        validateCmdOption(cmd, LBL_OUTPUT);
+        validateCmdOption(cmd, LBL_FRAMEWORK_NAME);
+    }
+
+    private static void validateCmdOption(CommandLine cmd, String option) {
+        if (!cmd.hasOption(option)) {
+            throw new IllegalArgumentException(option + " is missing");
         }
+    }
+
+    private static Path getMobilangAstCliArg(CommandLine cmd) {
+        String mobilangCliArg = cmd.getOptionValue(LBL_MOBILANG_AST);
+
+        return normalizePath(Path.of(mobilangCliArg));
+    }
+
+    private static Path normalizePath(Path path) {
+        return path.toAbsolutePath().normalize();
+    }
+
+    private static Path getOutputCliArg(CommandLine cmd) {
+        String outputCliArg = cmd.getOptionValue(LBL_OUTPUT);
+
+        return normalizePath(Path.of(outputCliArg));
+    }
+
+    private static String getFrameworkName(CommandLine cmd) {
+        return cmd.getOptionValue(LBL_FRAMEWORK_NAME);
     }
 
     private static void runAsc() 

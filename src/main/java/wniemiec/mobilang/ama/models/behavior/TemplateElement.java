b45a0ec00f327f42935dbbac0dcbd13cf526cc3f
William Niemiec <williamniemiec@hotmail.com>
2022-06-27 14:32:58 -0300

    Constructor doc

--- src/main/java/wniemiec/mobilang/ama/models/behavior/TemplateElement.java
+++ src/main/java/wniemiec/mobilang/ama/models/behavior/TemplateElement.java
@@ -2,7 +2,7 @@ package wniemiec.mobilang.ama.models.behavior;
 
 
 /**
- * Responsible for representing a template element from behavior code.
+ * Responsible for representing a template element from a template string.
  */
 public class TemplateElement implements Expression {
 
@@ -16,6 +16,13 @@ public class TemplateElement implements Expression {
     //-------------------------------------------------------------------------
     //		Constructor
     //-------------------------------------------------------------------------
+    /**
+     * Represents a template element from a template string.
+     * 
+     * @param       value Element content
+     * @param       tail True if the element is at the end of a template 
+     * string; false otherwise
+     */
     public TemplateElement(String value, boolean tail) {
         this.value = value;
         this.tail = tail;

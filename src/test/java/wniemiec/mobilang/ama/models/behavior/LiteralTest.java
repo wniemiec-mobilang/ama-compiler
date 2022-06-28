75e826791293d03d6a7d67f57cf170a19234e663
William Niemiec <williamniemiec@hotmail.com>
2022-06-28 15:54:19 -0300

    Literal - tests

---
+++ src/test/java/wniemiec/mobilang/ama/models/behavior/LiteralTest.java
@@ -0,0 +1,78 @@
+package wniemiec.mobilang.ama.models.behavior;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class LiteralTest {
+    
+    //-------------------------------------------------------------------------
+    //		Attributes
+    //-------------------------------------------------------------------------
+    private Literal literal;
+    private String value;
+    private boolean number;
+
+
+    //-------------------------------------------------------------------------
+    //		Test hooks
+    //-------------------------------------------------------------------------
+    @BeforeEach
+    void setUp() {
+        literal = null;
+        value = null;
+        number = false;
+    }
+
+
+    //-------------------------------------------------------------------------
+    //		Tests
+    //-------------------------------------------------------------------------
+    @Test
+    void testToCodeWithNumericValue() {
+        withNumericValue("7");
+        buildLiteral();
+        assertToCodeIs("7");
+    }
+
+    @Test
+    void testToCodeWithTextValue() {
+        withTextValue("some text");
+        buildLiteral();
+        assertToCodeIs("\"some text\"");
+    }
+
+    
+    //-------------------------------------------------------------------------
+    //		Methods
+    //-------------------------------------------------------------------------
+    private void withNumericValue(String value) {
+        this.value = value;
+        number = true;
+    }
+
+    private void buildLiteral() {
+        literal = number ? Literal.ofNumber(value) : Literal.ofString(value);
+    }
+
+    private void assertToCodeIs(String expectedCode) {
+        assertHasSameLine(expectedCode, literal.toCode());
+    }
+
+    private void assertHasSameLine(String expected, String obtained) {
+        Assertions.assertEquals(
+            removeWhiteSpaces(expected),
+            removeWhiteSpaces(obtained)
+        );
+    }
+
+    private String removeWhiteSpaces(String text) {
+        return text.replaceAll("[\\s\\t]+", "");
+    }
+
+    private void withTextValue(String value) {
+        this.value = value;
+        number = false;
+    }
+}

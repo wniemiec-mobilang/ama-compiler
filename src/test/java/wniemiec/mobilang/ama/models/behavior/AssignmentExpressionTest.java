e95ca34866d2371c9b2da88602c7d65043967a85
William Niemiec <williamniemiec@hotmail.com>
2022-06-29 21:02:12 -0300

    AssignmentExpression - tests

---
+++ src/test/java/wniemiec/mobilang/ama/models/behavior/AssignmentExpressionTest.java
@@ -0,0 +1,77 @@
+package wniemiec.mobilang.ama.models.behavior;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class AssignmentExpressionTest {
+
+    //-------------------------------------------------------------------------
+    //		Attributes
+    //-------------------------------------------------------------------------
+    private AssignmentExpression assignmentExpression;
+    private String operator;
+    private Expression left;
+    private Expression right;
+
+
+    //-------------------------------------------------------------------------
+    //		Test hooks
+    //-------------------------------------------------------------------------
+    @BeforeEach
+    void setUp() {
+        assignmentExpression = null;
+        operator = null;
+        left = null;
+        right = null;
+    }
+
+
+    //-------------------------------------------------------------------------
+    //		Tests
+    //-------------------------------------------------------------------------
+    @Test
+    void testToCodeWithTestAndConsequentAndAlternate() {
+        withOperator("=");
+        withLeft(new Identifier("index"));
+        withRight(Literal.ofNumber("0"));
+        buildAssignmentExpression();
+        assertToCodeIs("index = 0");
+    }
+
+    
+    //-------------------------------------------------------------------------
+    //		Methods
+    //-------------------------------------------------------------------------
+    private void withOperator(String operator) {
+        this.operator = operator;
+    }
+
+    private void withLeft(Expression expression) {
+        left = expression;
+    }
+
+    private void withRight(Expression expression) {
+        right = expression;
+    }
+
+    private void buildAssignmentExpression() {
+        assignmentExpression = new AssignmentExpression(operator, left, right);
+    }
+
+    private void assertToCodeIs(String expectedCode) {
+        assertHasSameLine(expectedCode, assignmentExpression.toCode());
+    }
+
+    private void assertHasSameLine(String expected, String obtained) {
+        Assertions.assertEquals(
+            removeWhiteSpaces(expected),
+            removeWhiteSpaces(obtained)
+        );
+    }
+
+    private String removeWhiteSpaces(String text) {
+        return text.replaceAll("[\\s\\t]+", "");
+    }
+}

9dcb78d6db2b7ae87e539f9d97325d6804af956d
William Niemiec <williamniemiec@hotmail.com>
2022-07-07 07:28:38 -0300

    IdentifierExpressionJsonParser - tests

---
+++ src/test/java/wniemiec/mobilex/ama/parser/screens/behavior/expression/IdentifierExpressionJsonParserTest.java
@@ -0,0 +1,77 @@
+package wniemiec.mobilex.ama.parser.screens.behavior.expression;
+
+import java.io.IOException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import wniemiec.mobilex.ama.models.behavior.Expression;
+import wniemiec.mobilex.ama.parser.exception.ParseException;
+
+
+class IdentifierExpressionJsonParserTest {
+    
+    //-------------------------------------------------------------------------
+    //		Attributes
+    //-------------------------------------------------------------------------
+    private IdentifierExpressionJsonParser parser;
+    private Expression parsedExpression;
+    private String name;
+
+
+    //-------------------------------------------------------------------------
+    //		Test hooks
+    //-------------------------------------------------------------------------
+    @BeforeEach
+    void setUp() {
+        parser = IdentifierExpressionJsonParser.getInstance();
+        name = null;
+    }
+
+
+    //-------------------------------------------------------------------------
+    //		Tests
+    //-------------------------------------------------------------------------
+    @Test
+    void testParseWithName() throws ParseException, IOException {
+        withName("sum");
+        doParsing();
+        assertParsedCodeIs("sum");
+    }
+
+
+    //-------------------------------------------------------------------------
+    //		Methods
+    //-------------------------------------------------------------------------
+    private void withName(String name) {
+        this.name = name;
+    }
+
+    private void doParsing() throws ParseException, IOException {
+        parsedExpression = parser.parse(buildIdentifierExpression());
+    }
+
+    private JSONObject buildIdentifierExpression() {
+        JSONObject expression = new JSONObject();
+
+        expression.put("type", "IdentifierExpression");
+        expression.put("name", name);
+
+        return expression;
+    }
+
+    private void assertParsedCodeIs(String code) {
+        assertHasSameLine(code, parsedExpression.toCode());
+    }
+
+    private void assertHasSameLine(String expected, String obtained) {
+        Assertions.assertEquals(
+            removeWhiteSpaces(expected),
+            removeWhiteSpaces(obtained)
+        );
+    }
+
+    private String removeWhiteSpaces(String text) {
+        return text.replaceAll("[\\s\\t]+", "");
+    }
+}

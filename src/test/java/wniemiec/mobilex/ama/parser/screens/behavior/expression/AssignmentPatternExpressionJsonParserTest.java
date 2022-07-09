7225949706f9f98b0c8bbf1df693796e9b204e2b
William Niemiec <williamniemiec@hotmail.com>
2022-07-09 06:36:21 -0300

    AssignmentPatternExpressionParser - tests

---
+++ src/test/java/wniemiec/mobilex/ama/parser/screens/behavior/expression/AssignmentPatternExpressionJsonParserTest.java
@@ -0,0 +1,95 @@
+package wniemiec.mobilex.ama.parser.screens.behavior.expression;
+
+import java.io.IOException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import wniemiec.mobilex.ama.models.behavior.Expression;
+import wniemiec.mobilex.ama.parser.exception.ParseException;
+
+
+class AssignmentPatternExpressionJsonParserTest {
+    
+    //-------------------------------------------------------------------------
+    //		Attributes
+    //-------------------------------------------------------------------------
+    private AssignmentPatternExpressionJsonParser parser;
+    private Expression parsedExpression;
+    private JSONObject left;
+    private JSONObject right;
+
+
+    //-------------------------------------------------------------------------
+    //		Test hooks
+    //-------------------------------------------------------------------------
+    @BeforeEach
+    void setUp() {
+        parser = AssignmentPatternExpressionJsonParser.getInstance();
+        parsedExpression = null;
+        left = null;
+        right = null;
+    }
+
+
+    //-------------------------------------------------------------------------
+    //		Tests
+    //-------------------------------------------------------------------------
+    @Test
+    void testParseWithLeftAndRight() throws ParseException, IOException {
+        withLeft(buildIdentifier("n1"));
+        withRight(buildIdentifier("n2"));
+        doParsing();
+        assertParsedCodeIs("n1 = n2");
+    }
+
+
+    //-------------------------------------------------------------------------
+    //		Methods
+    //-------------------------------------------------------------------------
+    private JSONObject buildIdentifier(String name) {
+        JSONObject expression = new JSONObject();
+
+        expression.put("type", "Identifier");
+        expression.put("name", name);
+
+        return expression;
+    }
+
+    private void withLeft(JSONObject expression) {
+        left = expression;
+    }
+
+    private void withRight(JSONObject expression) {
+        right = expression;
+    }
+
+    private void doParsing() throws ParseException, IOException {
+        parsedExpression = parser.parse(buildAssignmentPatternExpression());
+    }
+
+    private JSONObject buildAssignmentPatternExpression() {
+        JSONObject expression = new JSONObject();
+
+        expression.put("type", "AssignmentPatternExpression");
+        expression.put("left", left);
+        expression.put("right", right);
+
+        return expression;
+    }
+
+    private void assertParsedCodeIs(String code) {
+        assertHasSameLine(code, parsedExpression.toCode());
+    }
+
+    private void assertHasSameLine(String expected, String obtained) {
+        Assertions.assertEquals(
+            removeWhiteSpaces(expected),
+            removeWhiteSpaces(obtained)
+        );
+    }
+
+    private String removeWhiteSpaces(String text) {
+        return text.replaceAll("[\\s\\t]+", "");
+    }
+}
